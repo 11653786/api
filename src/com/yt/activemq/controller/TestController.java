b3e985a2b2881380101d378126df7e3300ed6f81
11653786@qq.com <yangtao>
2016-01-18 14:45:11 +0800

    人脸上传头像

--- src/com/yt/activemq/controller/TestController.java
+++ src/com/yt/activemq/controller/TestController.java
@@ -2,6 +2,7 @@ package com.yt.activemq.controller;
 
 import com.facepp.error.FaceppParseException;
 import com.facepp.http.PostParameters;
+import com.google.gson.reflect.TypeToken;
 import com.yt.activemq.entity.face.AllFack;
 import com.yt.activemq.entity.face.Face;
 import com.yt.activemq.entity.group.Group;
@@ -10,6 +11,7 @@ import com.yt.activemq.model.ResultObject;
 import com.yt.activemq.service.face.FaceService;
 import com.yt.activemq.util.FaceUtil;
 import com.yt.activemq.util.GsonUtil;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.util.List;
 import java.util.PriorityQueue;
 
 /**
@@ -91,6 +94,7 @@ public class TestController {
         }
         return resultObject;
     }
+
     //fd681267a13f70bff6b4185d09789613 person_id
     @RequestMapping(value = "/create/person", method = RequestMethod.GET)
     @ResponseBody
@@ -101,8 +105,8 @@ public class TestController {
         ResultObject resultObject = new ResultObject(true);
         try {
             JSONObject jsonObject = FaceUtil.getRequestInit().personCreate(new PostParameters().setPersonName(personName).setFaceId(faceId).setGroupId(groupId).setTag(tag));
-            Person person= GsonUtil.fromJson(jsonObject.toString(), Person.class);
-            resultObject.setMessage("创建成功,并且加入group: "+groupId);
+            Person person = GsonUtil.fromJson(jsonObject.toString(), Person.class);
+            resultObject.setMessage("创建成功,并且加入group: " + groupId);
             resultObject.setObject(person);
         } catch (FaceppParseException e) {
             resultObject.setSuccess(false);
@@ -110,6 +114,49 @@ public class TestController {
             e.printStackTrace();
         }
         return resultObject;
+    }
+
+    /**
+     * 比较两张脸的相似度,可以自定义设置
+     * @param faceId1
+     * @param faceId2
+     * @return
+     */
+    @RequestMapping(value = "/compare", method = RequestMethod.GET)
+    @ResponseBody
+    public ResultObject compare(@RequestParam(value = "faceId1", defaultValue = "a2a40ce6c57ef905dd1b5ec2b171fecd") String faceId1,
+                                @RequestParam(value = "faceId2", defaultValue = "933e330ff28b134cf9fd5aca27f32ec1") String faceId2) {
+        ResultObject resultObject = new ResultObject(true);
+        try {
+            JSONObject jsonObject = FaceUtil.getRequestInit().recognitionCompare(new PostParameters().setFaceId1(faceId1).setFaceId2(faceId2));
+            resultObject.setMessage("成功");
+            resultObject.setObject(jsonObject.toString());
+        } catch (FaceppParseException e) {
+            resultObject.setSuccess(false);
+            resultObject.setMessage("失败");
+            e.printStackTrace();
+        }
+        return resultObject;
+    }
+
+
+    @RequestMapping(value = "/getpersonlist", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Person> getPersonList() throws FaceppParseException, JSONException {
+        JSONObject jsonObject = FaceUtil.getRequestInit().infoGetPersonList();
+        //返回的的内容还有response_code
+        return GsonUtil.fromJson(jsonObject.get("person").toString(), new TypeToken<List<Person>>() {
+        }.getType());
+    }
+
+
+    @RequestMapping(value = "/getgrouplist", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Person> getGroupList() throws FaceppParseException, JSONException {
+        JSONObject jsonObject = FaceUtil.getRequestInit().infoGetGroupList();
+        //返回的的内容还有response_code
+        return GsonUtil.fromJson(jsonObject.get("group").toString(), new TypeToken<List<Person>>() {
+        }.getType());
     }
 
 
--- web/views/face.jsp
+++ web/views/face.jsp
@@ -21,6 +21,8 @@
     双人头像:http://img4.cache.netease.com/ent/2016/1/13/2016011321071571817.jpg<br>
     单人侧面头像:http://e.hiphotos.baidu.com/baike/w%3D480/sign=c6677012be12c8fcb4f3f7c5cc0292b4/83025aafa40f4bfb4f019c66044f78f0f736182a.jpg<br>
     单人正脸头像:http://upload.cbg.cn/2015/1120/1447990413976.jpg<br>
+    face2的图片:http://b.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=2c3038f79258d109c4b6a1b6e468e089/3bf33a87e950352a37387e815343fbf2b3118bda.jpg<br>
+    face2:杨颖头像:http://img5.duitang.com/uploads/item/201412/22/20141222193958_f4yiU.thumb.224_0.jpeg<br>
     图片url:<input name="url" id="url" style="width: 1000px;"  type="text" value="http://www.faceplusplus.com.cn/wp-content/themes/faceplusplus/assets/img/demo/9.jpg" />
     <input type="submit" value="提交" />
 </form>
